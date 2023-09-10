8973740a4a11b33a87b349e922492d8dbd1a7403
Md. Shohanur Rahman <shohan.sub.56@gmail.com>
2023-09-10 23:05:58 +0600

    bookings api added

---
+++ src/controllers/booking.controller.ts
@@ -0,0 +1,158 @@
+import { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import BookingModel from '../models/booking.model';
+import { handleError } from '../errors/handle.error';
+import UserModel from '../models/user.model';
+import TourPackageModel from '../models/tour_package.model';
+
+export default class BookingController {
+  constructor() {}
+
+  public async createABooking(req: Request, res: Response): Promise<void> {
+    try {
+      const { uid, tid } = req.params;
+      const userId = req?.user?._id;
+
+      if (!mongoose.Types.ObjectId.isValid(uid)) {
+        res.status(404).json({ message: 'User not found' });
+        return;
+      }
+
+      if (!mongoose.Types.ObjectId.isValid(tid)) {
+        res.status(404).json({ message: 'Tour Package not found' });
+        return;
+      }
+
+      if (uid !== userId?.toString()) {
+        res.status(403).json({ message: 'Forbidden' });
+        return;
+      }
+
+      await Promise.resolve().then(async () => {
+        const booking = await BookingModel.create({
+          user: uid,
+          tourPackage: tid,
+        });
+
+        await UserModel.findByIdAndUpdate(uid, {
+          $addToSet: {
+            bookings: booking._id,
+          },
+        });
+
+        await TourPackageModel.findByIdAndUpdate(tid, {
+          $addToSet: {
+            bookings: booking._id,
+          },
+        });
+
+        res.status(200).json(booking);
+      });
+    } catch (error: unknown) {
+      await handleError(error, res);
+    }
+  }
+
+  public async getAllBookingsForAnUser(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      const { uid } = req.params;
+      const userId = req?.user?._id;
+
+      if (!mongoose.Types.ObjectId.isValid(uid)) {
+        res.status(404).json({ message: 'User not found' });
+        return;
+      }
+
+      if (uid !== userId?.toString()) {
+        res.status(403).json({ message: 'Forbidden' });
+        return;
+      }
+
+      await Promise.resolve().then(async () => {
+        const bookings = await BookingModel.find({ user: uid });
+
+        res.status(200).json(bookings);
+      });
+    } catch (error: unknown) {
+      await handleError(error, res);
+    }
+  }
+
+  public async getABookingForAnUser(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      const { bid, uid } = req.params;
+      const userId = req?.user?._id;
+
+      if (!mongoose.Types.ObjectId.isValid(uid)) {
+        res.status(404).json({ message: 'User not found' });
+        return;
+      }
+
+      if (!mongoose.Types.ObjectId.isValid(bid)) {
+        res.status(404).json({ message: 'Booking not found' });
+        return;
+      }
+
+      if (uid !== userId?.toString()) {
+        res.status(403).json({ message: 'Forbidden' });
+        return;
+      }
+
+      await Promise.resolve().then(async () => {
+        const booking = await BookingModel.findById(bid);
+
+        res.status(200).json(booking);
+      });
+    } catch (error: unknown) {
+      await handleError(error, res);
+    }
+  }
+
+  public async deleteABooking(req: Request, res: Response): Promise<void> {
+    try {
+      const { bid, uid } = req.params;
+      const userId = req?.user?._id;
+
+      if (!mongoose.Types.ObjectId.isValid(uid)) {
+        res.status(404).json({ message: 'User not found' });
+        return;
+      }
+
+      if (!mongoose.Types.ObjectId.isValid(bid)) {
+        res.status(404).json({ message: 'Booking not found' });
+        return;
+      }
+
+      if (uid !== userId?.toString()) {
+        res.status(403).json({ message: 'Forbidden' });
+        return;
+      }
+
+      await Promise.resolve().then(async () => {
+        const booking = await BookingModel.findByIdAndDelete(bid);
+
+        res.status(200).json(booking);
+      });
+    } catch (error: unknown) {
+      await handleError(error, res);
+    }
+  }
+
+  public async getAllBookings(req: Request, res: Response): Promise<void> {
+    try {
+      await Promise.resolve().then(async () => {
+        const bookings = await BookingModel.find();
+
+        res.status(200).json(bookings);
+      });
+    } catch (error: unknown) {
+      await handleError(error, res);
+    }
+  }
+}
--- src/routes/booking.route.ts
+++ src/routes/booking.route.ts
@@ -1,20 +1,46 @@
 import express, { Router } from 'express';
+import AuthMiddleware from '../middlewares/auth.middleware';
+import BookingController from '../controllers/booking.controller';
 
 const bookingRouter: Router = express.Router();
 
-// get all bookings
-bookingRouter.get('/');
-
-// get all bookings for an user
-bookingRouter.get('/users/:id');
+const authMiddleware = new AuthMiddleware();
+const bookingInstance = new BookingController();
 
 // create a booking for an user
-bookingRouter.post('/users/:id/tour_packages/:id');
+bookingRouter.post(
+  '/users/:uid/tour_packages/:tid',
+  authMiddleware.verifyUser,
+  bookingInstance.createABooking
+);
 
-// delete a booking for an user
-bookingRouter.delete('/users/:id/tour_packages/:id');
+// get all bookings for an user
+bookingRouter.get(
+  '/users/:uid',
+  authMiddleware.verifyUser,
+  bookingInstance.getAllBookingsForAnUser
+);
 
 // get a booking for an user
-bookingRouter.get('/:id/users/:id');
+bookingRouter.get(
+  '/:bid/users/:uid',
+  authMiddleware.verifyUser,
+  bookingInstance.getABookingForAnUser
+);
+
+// delete a booking for an user
+bookingRouter.delete(
+  '/:bid/users/:uid',
+  authMiddleware.verifyUser,
+  bookingInstance.deleteABooking
+);
+
+// get all bookings
+bookingRouter.get(
+  '/',
+  authMiddleware.verifyUser,
+  authMiddleware.checkAdminRole,
+  bookingInstance.getAllBookings
+);
 
 export default bookingRouter;
